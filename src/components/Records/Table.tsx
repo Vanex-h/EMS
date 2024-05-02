0d6e612c8089433b610909d6ab59b8e3c6a26a47
Vanex-h <vanessahirwa5@gmail.com>
2024-05-02 18:06:50 +0200

    Complete

--- src/components/Records/Table.tsx
+++ src/components/Records/Table.tsx
@@ -2,10 +2,15 @@
 
 const Table = () => {
     return (
-        <div className="h-full p-3">
-       <table className="table-fixed w-full h-auto bg-white border-collapse border border-gray-300">
+        <div className="h-full p-3 text-[#48505E]">
+          <div className="bg-white h-16 flex flex-row justify-between p-3 items-center">
+            <h2 className="text-xl font-medium text-[#444444]">Employee</h2>
+            <button className="border px-6 py-4 bg-[#1366D9] text-white text-sm rounded-md hover:bg-white hover:border-[#1366D9] hover:text-[#1366d9]">Add Employee +</button>
+          </div>
+       <table className="table-fixed w-full h-auto bg-white border-collapse border border-transparent">
+        
   <thead>
-    <tr className="text-[#667085] border-b border-gray-300">
+    <tr className="text-[#667085] border-b border-transparent">
       <th className=" py-2">ID</th>
       <th className=" py-2">FirstName</th>
       <th className=" py-2">LastName</th>
@@ -20,7 +25,7 @@ const Table = () => {
     </tr>
   </thead>
   <tbody>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -33,7 +38,7 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -46,7 +51,7 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -59,7 +64,7 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -72,7 +77,7 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -85,7 +90,7 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -98,7 +103,7 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
+    <tr className=" border-b border-transparent">
       <td className=" py-2">102</td>
       <td className=" py-2">Samanta</td>
       <td className=" py-2">ISHIMWE</td>
@@ -111,24 +116,13 @@ const Table = () => {
       <td className=" py-2">Envy</td>
       <td className="px-4 py-2">3400</td>
     </tr>
-    <tr className=" border-b border-gray-300">
-      <td className=" py-2">102</td>
-      <td className=" py-2">Samanta</td>
-      <td className=" py-2">ISHIMWE</td>
-      <td className=" py-2 text-sm">120000710913307</td>
-      <td className=" py-2">0788888888</td>
-      <td className=" py-2 text-[13px]">samanta@gmail.com</td>
-      <td className=" py-2">Human resource</td>
-      <td className=" py-2">Manager</td>
-      <td className=" py-2">HP</td>
-      <td className=" py-2">Envy</td>
-      <td className="px-4 py-2">3400</td>
-    </tr>
-   
-
-    
   </tbody>
 </table>
+<div className="h-16 flex flex-row p-3 bg-white justify-between w-[100%]">
+  <button className="border px-3 text-sm hover:bg-[#48505E] hover:text-white rounded-md">Previous</button>
+  <div className="text-[15px] flex flex-row items-center justify-evenly w-24">Page <br /><div className="font-medium">1</div> of <div className="font-medium">10</div></div>
+  <button className="border px-3 text-sm hover:bg-[#48505E] hover:text-white rounded-md">Next</button>
+</div>
 
         </div>
     );
