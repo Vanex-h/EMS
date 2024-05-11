884bf3f07375154e57751018b14e4aec17862dae
Vanex-h <vanessahirwa5@gmail.com>
2024-05-11 23:05:00 +0200

    Better

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,12 +1,18 @@
-const express= require('express');
-const router=  express.Router();
+const express = require("express");
+const router = express.Router();
 
-const { createUser, updateUser, deleteUserById, getAll, getUserById } = require('../controllers/users.controller');
-const { userLogin } = require('../controllers/users.controller');
-router.post("/createUser", createUser)
-router.put("/update/:id", updateUser)
-router.delete('/delete/:id', deleteUserById)
-router.get("/all", getAll)
-router.get("/User/:id", getUserById)
-router.post("/login", userLogin)
-module.exports= router;
+const {
+  createUser,
+  updateUser,
+  deleteUserById,
+  getAll,
+  getUserById,
+} = require("../controllers/users.controller");
+const { userLogin } = require("../controllers/users.controller");
+router.post("/createUser", createUser);
+router.put("/update/:id", updateUser);
+router.delete("/delete/:id", deleteUserById);
+router.get("/all", getAll);
+router.get("/User/:id", getUserById);
+router.post("/login", userLogin);
+module.exports = router;
